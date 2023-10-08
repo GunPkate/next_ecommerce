62535ae0e5d1265d1f4d80651eb1e280edbc7afe
GunPKate <gunPK>
2023-10-08 20:00:04 +0700

    log in btn

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={'bg-red-500'}>123</div>
+    <div className={'bg-blue-500 w-screen h-screen flex items-center'}>
+      <div className="text-center w-full">
+        <button className='bg-white p-2 px-4 rounded-md'>Login with Google</button>
+      </div>
+    </div>
   )
 }
