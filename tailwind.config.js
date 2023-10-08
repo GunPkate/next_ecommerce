18a5d2d191d84fdc331bac8b533a686ba75647c1
GunPKate <gunPK>
2023-10-08 13:37:55 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
