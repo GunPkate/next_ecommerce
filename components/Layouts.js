64281a3638b49c06c97bfc672660085ec1fcd437
GunPKate <gunPK>
2023-10-10 22:08:19 +0700

    dash board user section

--- components/Layouts.js
+++ components/Layouts.js
@@ -19,7 +19,19 @@ export default function Layout({children}) {
     <div className={'bg-blue-500 w-screen h-screen flex'}>
         <Nav/>
         <div className="bg-white flex-grow mt-2 mr-2 mb-2 p-4 rounded-lg">
-          {children}
+          {/* {children} */}
+          <div className='text-blue-900 flex justify-between'>
+            <h2>
+                Hello, <b> {session?.user?.name} </b>
+            </h2>
+            <div className='flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden'>
+                <img src={session?.user?.image} className='w-8 h-8' /> 
+                <span className='px-2'>
+                    {session?.user?.name}
+                </span>
+            </div>
+
+          </div>
         </div>
     </div>
   )
