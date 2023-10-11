1e1f56e14613e73bbecc7a7884303e52c62e6652
GunPKate <gunPK>
2023-10-11 20:17:44 +0700

    form add new product

--- pages/products/new.js
+++ pages/products/new.js
@@ -2,6 +2,13 @@ import Layout from "@/components/Layouts";
 
 export default function NewProduct(){
    return <Layout>
+        <h1>New Product</h1>
+        <label>Product name</label>
         <input type="text" placeholder="Product Name"/>
+        <label>Description</label>
+        <textarea placeholder="Description"/>
+        <label>Price (Unit)</label>
+        <input type="text" placeholder="Price"/>
+        <button className="btn-primary">Save</button>
    </Layout> 
 }
