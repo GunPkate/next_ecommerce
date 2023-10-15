fd4035b922172579744a6ff147006e44ef2fe769
GunPKate <gunPK>
2023-10-15 11:51:35 +0700

    api get category

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,11 +1,18 @@
+import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/categories";
 
-export default function handel(req,res){
+export default async function handel(req,res){
     const {method} = req
+    await mongooseConnect();
 
     if(method === "POST"){
         const {name} = req.body
-        const categoryDoc = Category.create({name})
+        const categoryDoc = await Category.create({name})
         res.json(categoryDoc)
+    }
+
+    if(method === "GET"){
+        const categoryDoc = await Category.find();
+        res.json(categoryDoc);
     }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,15 +1,28 @@
 import Layout from "@/components/Layouts";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function categories(){
-    const [name,setName] = useState();
+    const [name,setName] = useState('');
+    const [categories,setCategories] = useState([]);
+
     async function saveCategory(ev){
         ev.preventDefault();
         await axios.post("/api/categories",{name})
         console.log(name)
         setName('');
+        fetchCategory();
     }
+
+    async function fetchCategory(){
+        // axios.get("/api/categories").then(result=> console.log(result))
+        axios.get("/api/categories").then(result=>setCategories(result.data))
+
+    }
+
+    useEffect(()=>{
+        fetchCategory();
+    },[])
     
     return (
     <Layout>
@@ -25,6 +38,21 @@ export default function categories(){
                 />
             <button type= "submit" className="btn-primary py-1">Save</button>
         </form>
+
+        <table className="basic mt-4">
+            <thead>
+                <tr>
+                    <td>Category Name</td>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    categories.length > 0 && categories.map(category=>(
+                        <tr>{category.name}</tr>
+                    ))
+                }
+            </tbody>
+        </table>
     </Layout>
     )
 }
